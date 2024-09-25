3c189f3e72f55ec69b9b754731995ff7d41fc991
Iris Flystam <badde00@gmail.com>
2024-09-25 08:50:04 +0200

    basic reqs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardList = document.querySelector(".cards");
 
-console.log(data);
+  data.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("card");
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+    listItem.innerHTML = `
+      <h2 class="card--title">${item.name}</h2>
+  <img
+    width="256"
+    class="card--img"
+    src=${item.sprites.other["official-artwork"].front_default}
+  />
+  <ul class="card--text">
+    <li>HP: ${getStatByName(item.stats, "hp")}</li>
+    <li>ATTACK: ${getStatByName(item.stats, "attack")}</li>
+    <li>DEFENSE: ${getStatByName(item.stats, "defense")}</li>
+    <li>SPECIAL-ATTACK: ${getStatByName(item.stats, "special-attack")}</li>
+    <li>SPECIAL-DEFENSE: ${getStatByName(item.stats, "special-defense")}</li>
+    <li>SPEED: ${getStatByName(item.stats, "speed")}</li>
+  </ul>
+    `;
+
+    cardList.appendChild(listItem);
+  });
+});
+
+function getStatByName(stats, statName) {
+  let correctStat = stats.find((stat) => stat.stat.name === statName);
+  return correctStat ? correctStat.base_stat : null;
+}
